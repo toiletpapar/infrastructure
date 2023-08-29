cf3e376b0fc2402de39c6eb72e4531ce8494887e
Tyler Poon <tylerbill99@hotmail.com>
2023-08-28 20:21:08 -0400

    update and deploy

--- smithers-server/build.sh
+++ smithers-server/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME=us-east1-docker.pkg.dev/budget-server-370523/docker-smithers/smithers-server:1.15
+IMAGE_NAME=us-east1-docker.pkg.dev/budget-server-370523/docker-smithers/smithers-server:1.16
 GIT_URL=https://github.com/toiletpapar/smithers-server.git
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "SCRIPT DIR: $SCRIPT_DIR"
@@ -10,4 +10,4 @@ docker buildx build -t $IMAGE_NAME --build-context repo=$GIT_URL $SCRIPT_DIR
 
 # In cmd
 # gcloud auth configure-docker us-east1-docker.pkg.dev
-# docker push us-east1-docker.pkg.dev/budget-server-370523/docker-smithers/smithers-server:1.15
+# docker push us-east1-docker.pkg.dev/budget-server-370523/docker-smithers/smithers-server:1.16
